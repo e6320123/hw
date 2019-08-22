2cca6f1a971b245e88895187ea00f8463b7cd43b
e6320123 <52902368+e6320123@users.noreply.github.com>
2019-08-22 21:09:47 +0800

    Add files via upload

---
+++ hw02.php
@@ -0,0 +1,34 @@
+<?php
+
+$poker=array();     //??????
+
+for ($i=0; $i <52 ; $i++) { //????0~51????
+    $poker[]=$i;
+}
+
+for ($i=0; $i <52 ; $i++) { 
+    $rune=rand($i,51);      //$i~51???????$i??
+    //????
+    $temp=$poker[$i];
+    $poker[$i]=$poker[$rune];
+    $poker[$rune]=$temp;
+}
+ 
+$players=[[],[],[],[]];     //??4???
+foreach($poker as $i=>$card){
+    $players[$i%4][(int)($i/4)]=$card;  //?????
+}
+echo"<table border='1' width='60%'>";
+for($i=0;$i<4;$i++){    //?4???
+    $j=$i+1;
+    echo"<tr><td>玩家{$j}</td>";      //????1~4
+    foreach($players[$i] as $j){
+        echo"<td>";
+        echo"$j";           //??????
+        echo"</td>";
+    }
+    echo"</tr>";
+}
+echo"</table>";
+
+?>
